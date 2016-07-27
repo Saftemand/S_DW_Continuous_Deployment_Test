9ccab887b7c0dd327cfe3ba8eef5c6483948bd95
Saftemand <michaelmantonsen@gmail.com>
2016-07-27 21:38:59 +0200

    changed some cshtmls againagain

--- S_DW_Continuous_Deployment_test/CustomModules/HelloWorld/HelloWorld.cs
+++ S_DW_Continuous_Deployment_test/CustomModules/HelloWorld/HelloWorld.cs
@@ -11,7 +11,7 @@ namespace CustomModules.HelloWorld
         public override string GetContent()
         {
             //TODO: Add code here
-            Debug.WriteLine("hej hej med dig, hvordan går det idag");
+            Debug.WriteLine("hej hej med dig, hvordan går det idag med dig");
             return Properties["HelloText"].ToString();
         }
 
